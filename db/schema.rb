30e2c49a1f7adc3fd4fec7665c4a88fca63ba07b
Lionel Kubwimana <lionel.kubwimana@gmail.com>
2017-11-21 18:52:02 +0100

    added markdown for content

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121135820) do
+ActiveRecord::Schema.define(version: 20171121162029) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.string "content"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-require 'faker'
+# require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -7,8 +7,32 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-10.times do
-  title = Faker::Food.dish
-  content = Faker::Lorem.paragraph
-  Article.create(title: title, content: content)
-end
+# 10.times do
+#   title = Faker::Food.dish
+#   content = Faker::Lorem.paragraph
+#   Article.create(title: title, content: content)
+# end
+
+Article.delete_all
+
+Article.create(
+  title: "My Very First Article",
+  content:
+  %Q{### There Is Something You Should Know!
+
+  This is my very first Article using markdown!
+
+  How do you like it?  I learned this from [RichOnRails.com](http://richonrails.com/articles/rendering-markdown-with-redcarpet)!}
+)
+
+Article.create(
+  title: "My Second Article",
+  content:
+  %Q{### My List of Things To Do!
+
+  Here is the list of things I wish to do!
+
+  * write more Articles
+  * write even more Articles
+  * write even more Articles!}
+)
